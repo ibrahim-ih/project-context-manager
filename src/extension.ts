c7fbba0a1523ea937c86309f3c9af564e70553f2
Ibrahim Haouari <ibrahim.haouari@gmail.com>
2025-05-25 16:15:01 +0200

    init add projects

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,35 @@
-// # Entry point
-
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "project-context-manager" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('project-context-manager.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from project-context-manager!');
-	});
-
-	context.subscriptions.push(disposable);
+function getSubfolders(workspacePath: string): string[] {
+    return fs.readdirSync(workspacePath)
+        .map(name => path.join(workspacePath, name))
+        .filter(p => fs.existsSync(p) && fs.statSync(p).isDirectory());
 }
 
-// This method is called when your extension is deactivated
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('projectContextManager.selectProject', async () => {
+        const workspaceFolders = vscode.workspace.workspaceFolders;
+        if (!workspaceFolders || workspaceFolders.length === 0) {
+            vscode.window.showErrorMessage('No workspace folder is open.');
+            return;
+        }
+
+        const rootPath = workspaceFolders[0].uri.fsPath;
+        const subfolders = getSubfolders(rootPath);
+
+        const selected = await vscode.window.showQuickPick(subfolders, {
+            placeHolder: 'Select a project folder'
+        });
+
+        if (selected) {
+            vscode.window.showInformationMessage(`Selected project: ${selected}`);
+            // You can now act based on the selected folder
+        }
+    });
+
+    context.subscriptions.push(disposable);
+}
+
 export function deactivate() {}
